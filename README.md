6c7c0d6507ae5c0193e1744e78222cdae4e04419
ntorre <ntorre@gaya.fr>
2016-04-29 12:28:52 +0200

    [TASK] Init report website test phantomjs

---
+++ README.md
@@ -0,0 +1,3 @@
+# Report website test with PhantomJS
+
+> Report for list of sites (Array sites) the status, loading time and screenshot
---
+++ testWebsite_phantomjs.js
@@ -0,0 +1,174 @@
+/**
+ * Script PhantomJS
+ * Author: nicolastorre
+ * Description: report for list of sites (Array sites) the status, loading time and screenshot
+ * Licence: MIT
+ * 
+ */
+	'use strict';
+
+	var webpage = require('webpage');
+
+	var sites = [
+	    "http://www.google.com"
+	];
+
+	var controller = function() {
+		this.init();
+	};
+
+	controller.prototype = {
+
+		init: function() {
+			console.log('Init script parameters');
+
+			this.pages = [];
+			this.outputHTML = "";
+			this.index = 0;
+			this.start_time_global = Date.now();
+			this.outputHTML = "<!doctype html><html lang='fr'><head><meta charset='utf-8'><title>PahntomJS Website Report</title><style>table, tr, th, td { border: 1px solid black; border-collapse: collapse;}</style></head><body><h1>PhantomJS Website Report</h1><table><thead><tr><th>URL du site</th><th>Statut</th><th>Loading time</th><th>Request</th><th>Screenshot</th></tr></thead>";
+			
+			this.testURL();
+
+		},
+
+		initParam: function() {
+			this.time_start = Date.now();
+			this.error_buffer = [];
+			this.ressources_buffer = [];
+			this.image_counter = 0;
+			this.html_counter = 0;
+			this.scripts_counter = 0;
+			this.styles_counter = 0;
+			this.other_counter = 0;
+			this.time_loadStart = 0;
+			this.time_loadEnd = 0;
+			
+		},
+
+		testURL: function() {
+			var that = this;
+			var bgColor;
+			var color;
+			var msgStatus;
+			
+			this.initParam();
+
+			if (this.index < sites.length) {
+				console.log("URL: "+sites[this.index]);
+
+				this.pages[this.index] = webpage.create();
+				this.pages[this.index].viewportSize = {width: 1280, height: 800};
+				
+				this.pages[this.index].onResourceReceived = function (response) {
+					that.onResourceReceived(response);
+				};
+
+				this.pages[this.index].onError = function (msg, trace) {
+					that.onError(msg, trace);
+				};
+
+				this.pages[this.index].onLoadStarted = function() {
+					var now = new Date();
+					that.time_loadStart = now.getTime();
+				};
+
+				this.pages[this.index].onLoadFinished = function(status) {
+					var now = new Date();
+					that.time_loadEnd = now.getTime();
+				};
+				
+				this.pages[this.index].open(sites[this.index], function (status) {
+					
+					if ('success' === status && 0 === that.error_buffer.length) {
+						bgColor = "#00FF00";
+						color = "#000000";
+						msgStatus = "SUCCESS";
+					} else {
+						bgColor = "#FF0000";
+						color = "#FFFFFF";
+						msgStatus = "FAIL";
+					}
+
+					that.addToOutputHTML(bgColor, color, msgStatus);
+					that.index = that.index + 1;
+					that.testURL();
+				});
+
+			} else {
+				this.exportOutputPDF();
+			}
+		},
+		
+		exportOutputPDF: function() {
+
+			this.outputHTML += "</table></body></html>";
+
+			var output = webpage.create();
+            output.paperSize = {format: 'A4', orientation: 'portrait', margin: '1cm' };
+            output.setContent(this.outputHTML,"");
+
+            console.log("Analysis done ...");
+            
+            setTimeout(function () {
+                output.render('WebsiteReport.pdf', {format: 'pdf', quality: '100'});
+                console.log('Report generated automatically in ' + (Date.now() - this.start_time_global) + ' ms for ' + sites.length + ' sites (with PhantomJS version ' + phantom.version.major + '.' + phantom.version.minor + '.' + phantom.version.patch +')');
+                phantom.exit();
+            }, 5000);
+		},
+		
+		addToOutputHTML: function(bgColor, color, msgStatus) {
+
+		        var img = this.pages[this.index].renderBase64('PNG');
+
+				this.outputHTML += '<tr>'+
+				'<td style="background-color: '+bgColor+'; color: '+color+';">'+msgStatus+'</td>'+
+				'<td><a href="' + sites[this.index] + '" target="_blank">' + sites[this.index] + '</a></td>'+
+                '<td>' + (this.time_loadEnd - this.time_loadStart) + ' ms</td>'+
+		        '<td>Images : ' + this.image_counter + '<br>Scripts : ' + this.scripts_counter + '<br>Styles : ' + this.styles_counter + '<br>HTML : ' + this.html_counter + '<br>Autres : ' + this.other_counter+'</td>'+
+		        '<td><img width="200" src="data:image/png;base64,' + img + '"></td>'+
+		        '</tr>';
+		        
+		},
+
+		onResourceReceived: function(response) {
+			switch (response.contentType) {
+				case 'image/png':
+				case 'image/jpeg':
+				case 'image/gif':
+				    this.image_counter = this.image_counter + 1;
+				    break;
+				case 'text/html; charset=UTF-8':
+				    this.html_counter = this.html_counter + 1;
+				    break;
+				case 'text/css':
+				    this.styles_counter = this.styles_counter + 1;
+				    break;
+				case 'text/javascript':
+				case 'text/javascript; charset=UTF-8':
+				case 'application/x-javascript':
+				case 'application/javascript':
+				case 'application/javascript; charset=utf-8':
+
+				    this.scripts_counter = this.scripts_counter + 1;
+				    break;
+				default:
+				    this.ressources_buffer.push('Response (#' + response.id + ', stage "' + response.stage + '"): ' + JSON.stringify(response));
+				    this.other_counter = this.scripts_counter + 1;
+				    break;
+			}
+		},
+
+		onError: function(msg, trace) {
+			this.error_buffer = [msg];
+			if (trace) {
+				trace.forEach(function (t) {
+					this.error_buffer.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+				});
+			}	
+		}
+
+	};
+	
+	var phantomJSTest = new controller();
+
